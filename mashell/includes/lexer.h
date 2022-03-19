5230e753f268ea70b6a9455c93aef0f321c6ec0f
Milton Rojas echeverri <mrojas-e@1-A-16.42heilbronn.de>
2022-03-19 13:02:30 +0100

    some changes

--- mashell/includes/lexer.h
+++ mashell/includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: mrojas-e <mrojas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 11:44:12 by mrojas-e          #+#    #+#             */
-/*   Updated: 2022/03/11 19:42:43 by mrojas-e         ###   ########.fr       */
+/*   Updated: 2022/03/19 13:02:12 by mrojas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	str_builder_destroy(t_str_builder *build);
 void	str_builder_join_char(t_str_builder *build, char c);
 void	str_builder_join_str(t_str_builder *build, char *s);
 char	*str_builder_join_finalize(t_str_builder *build);
+
 
 
 #endif
