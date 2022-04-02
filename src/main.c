45f9b7a6f26f0c35c3be1d14e4073854150659b3
S4t0ri <miltonre2@gmail.com>
2022-04-02 19:54:46 +0200

    Normed and echo command ready

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mrojas-e <mrojas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 17:35:14 by mrojas-e          #+#    #+#             */
-/*   Updated: 2022/04/02 18:19:53 by mrojas-e         ###   ########.fr       */
+/*   Updated: 2022/04/02 19:43:17 by mrojas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,20 +40,20 @@ int	main(int argc, char *argv[], char *envp[])
 		if (lexer_struct.contents == NULL)
 			exit_readline_fail();
 		add_history(lexer_struct.contents);
-		//printf("Readline input is: %s\n", lexer_struct.contents); //
-		//printf("System command exec:\n"); //
-		//system(lexer_struct.contents); //
+		printf("Readline input is: %s\n", lexer_struct.contents); //
+		printf("System command exec:\n"); //
+		system(lexer_struct.contents); //
 		lexer_done = lexer(&lexer_struct);
-		//print_lexer_blocks(lexer_done); //
+		print_lexer_blocks(lexer_done); //
 		if (pipe_redir_error(lexer_done) == true) //need to remake to handle empty token! and implement in parser
 			continue;
 		expander(lexer_done); // need to handle empty string in executor!!. also exit status of successfull command needs to be 0
-		//print_lexer_blocks(lexer_done); //
+		print_lexer_blocks(lexer_done); //
 		parser_done = parser(lexer_done);
 		if (parser_done == NULL)
 			continue;
 		cmd_echo(parser_done);
-		//print_parser_blocks(parser_done); //
+		print_parser_blocks(parser_done); //
 		free_parser_blocks(parser_done);
 	//	system("leaks minishell"); //
 	//	break ; //
