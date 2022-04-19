edd1d1975b69a3685bcd1b284a47b385e0fc21e2
S4t0ri <miltonre2@gmail.com>
2022-04-19 22:13:09 +0200

    cmd_env and cmd_pwd

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mrojas-e <mrojas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 11:44:12 by mrojas-e          #+#    #+#             */
-/*   Updated: 2022/04/19 21:07:50 by mrojas-e         ###   ########.fr       */
+/*   Updated: 2022/04/19 22:09:34 by mrojas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ void	print_env(void);
 
 /*============BUILTINS===============*/
 
-void	cmd_pwd(void);
+bool	cmd_pwd(t_exec_block *cmd_pwd);
 void	cmd_echo(t_exec_block *echo);
 bool	cmd_cd(t_exec_block *cd);
 bool	cmd_env(t_exec_block *cmd_env);
--- src/builtins/cmd_export.c
+++ src/builtins/cmd_export.c
@@ -1,38 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cmd_pwd.c                                          :+:      :+:    :+:   */
+/*   cmd_export.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrojas-e <mrojas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/03 13:51:12 by mrojas-e          #+#    #+#             */
-/*   Updated: 2022/04/19 22:11:48 by mrojas-e         ###   ########.fr       */
+/*   Created: 2022/04/19 22:02:58 by mrojas-e          #+#    #+#             */
+/*   Updated: 2022/04/19 22:05:10 by mrojas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-bool	cmd_pwd(t_exec_block *cmd_pwd)
+void	cmd_export(t_env *cmd_export)
 {
-	char	*cwd;
+	char *str;
 
-	if (cmd_pwd->arg !=  NULL)
-		return (handle_error(
-			"Mi[shell]in: pwd: toooo much too handle\n",
-			EXIT_STD_ERROR));
-	cwd = getcwd(NULL, 0);
-	if (cwd == NULL)
-	{
-		perror("Error: pwd");
-		g_exit_status = 2;
-		return (true);
-	}
-	else
-	{
-		ft_putstr_fd(cwd, STDOUT_FILENO);
-		ft_putstr_fd("\n", STDOUT_FILENO);
-		g_exit_status = 0;
-	}
-	free(cwd);
-	return (false);
+	init_env_node(str, cmd_export);
 }
