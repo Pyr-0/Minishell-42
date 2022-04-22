bc798b06d19e64385f1b8a5fedb3c5f596cd2cbb
S4t0ri <miltonre2@gmail.com>
2022-04-22 13:31:43 +0200

    error handlers have color now & Files were normed

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,11 +6,7 @@
 /*   By: mrojas-e <mrojas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 21:30:46 by shaas             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/04/22 13:24:32 by mrojas-e         ###   ########.fr       */
-=======
-/*   Updated: 2022/04/21 19:43:21 by shaas            ###   ########.fr       */
->>>>>>> 4f6bffb7ef9206d1aebfaef31d7ece5e99f2396f
+/*   Updated: 2022/04/22 13:29:49 by mrojas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +40,10 @@ void	executor(t_exec_block *exec_blocks)
 	{
 		if (is_inbuilt(i_exec) == true)
 			handle_inbuilt(i_exec);
-<<<<<<< HEAD
-=======
 		else if (i_exec->cmd != NULL)
 		{
 			cmd_path = find_cmd_path(i_exec->cmd);
 		}
->>>>>>> 4f6bffb7ef9206d1aebfaef31d7ece5e99f2396f
 		i_exec = i_exec->next;
 	}
 	free_close_exec_blocks(exec_blocks);
