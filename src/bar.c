dcc1d80506fed515b7c3c8f72cc7131757a16b97
S4t0ri <miltonre2@gmail.com>
2022-04-30 03:34:55 +0200

    LOADING . . .

--- src/bar.c
+++ src/bar.c
@@ -6,7 +6,7 @@
 /*   By: mrojas-e <mrojas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 03:08:55 by mrojas-e          #+#    #+#             */
-/*   Updated: 2022/04/30 03:09:11 by mrojas-e         ###   ########.fr       */
+/*   Updated: 2022/04/30 03:34:34 by mrojas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 
 void progress()
 {
-    int i = 0;
-    char bar[102];
-    memset(bar,0,102*sizeof(char));
-    const char* lable="|/-\\";
+	int			i;
+	char		bar[102];
+	const char*	lable;
 
-    printf("\e[36m                                    LOADING PLEASE WAIT ...\e[0m\n");
-    while(i <= 100)
-    {
-        bar[i] = '#';    
-        //printf("\r\e[36m [%d%%] [%c]\n",i,lable[i%4]); line by line %
-        printf("\r\e[4;35m\e[40m\e[1;98m[%-2s]\e[0m\e[36m [%d%%] [%c]",bar,i,lable[i%4]);
-        usleep(19999);
-    	fflush(stdout);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	lable = "|/-\\";
+	ft_memset(bar, 0, 102 * sizeof(char));
+	printf("\e[36m                                    LOADING PLEASE WAIT ...\e[0m\n");
+	while(i <= 100)
+	{
+		bar[i] = '#';
+		//printf("\r\e[36m [%d%%] [%c]\n",i,lable[i%4]); line by line %
+		printf("\r\e[4;35m\e[40m\e[1;98m[%-2s]\e[0m\e[36m [%d%%] [%c]",bar,i,lable[i%4]);
+		usleep(30000);
+		fflush(stdout);
+		i++;
+	}
+	printf("\n");
 }
